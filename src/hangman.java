85887ceeacd6ce157bdeda728dcfb1412698fab6
Sinan Demir <sinanmaraz4@gmail.com>
2023-11-12 23:15:43 +0300

    Hangman is updated.

--- src/hangman.java
+++ src/hangman.java
@@ -14,7 +14,7 @@ public class hangman {
         String city = cities[num];
         int cL = city.length();
         System.out.printf("****** %d Harfli bir şehir ******\n", cL);
-        System.out.println(city);
+
         char[] word = new char[cL];
 
         // Kelime sayısı kadar alt tire olan dizi atar
@@ -34,7 +34,7 @@ public class hangman {
 
             // KULLANICIDAN GİRDİ ALIR
             Scanner scanner = new Scanner(System.in);
-            System.out.print("\nHarfi veya Kelime tahmininizi girin : ");
+            System.out.print("\nHarfi Veya Kelimeyi Tahmin Ediniz : ");
             String guess = scanner.nextLine();
 
             // Geçen süreyi hesaplar
@@ -55,6 +55,7 @@ public class hangman {
             else if (guess.toUpperCase().equals(city)) {
                 System.out.print("*** TEBRİKLER BİLDİNİZ ***" );
                 System.out.printf("\nGeçen süre : %.2f Saniye\n",passTimeFloat);
+                System.out.println("Skorunuz : "+Score(passTimeFloat,vote));
                 break; }
 
             /* kullanıcı harf girmişsse Şehrin her bir karakterini tarar ve eşleme yaparsa bunu daha önce oluşturulan
@@ -77,18 +78,21 @@ public class hangman {
             // Kullanıcının bilmediği her harf için Adam asılır
             if (!corret) {
                 vote --;
+                System.out.println("\n**** YANLIŞ **** ");
                 hangmanImage(vote);
             }
+            if (corret) System.out.println("**** DOĞRU ****");
             if (vote==0) {
                 System.out.println("\n *** KAYBETTİNİZ ***");
                 System.out.println("Doğru Cevap : "+city);
-
+                System.out.println("Skorunuz : "+Score(passTimeFloat,vote));
             }
          }
             // olaki kullanıcı Char dışında bir karakter girerse Onu nazikçe uyarıyoruz :)
             else System.out.println("\n**** Harf Al Veya Şehri Tahmin ET ! ****\n");
        }
     }
+
     public static void hangmanImage(int mistake){
         switch(mistake) // her yanlış bilinen harf için adam fiğüri çizdirir.
         {  case 4 : System.out.print("\n\t\t|\n"); break;
@@ -98,4 +102,38 @@ public class hangman {
             case 0 : System.out.print("\n\t\t|\n\t\tØ\n\t  --|--\n\t   / \\ "); break;
         }
     }
+
+
+    // Zamana ve yapılan yanlışa göre Puanlama sistemi
+    public static int Score(float time,int mistake){
+
+        // ilk önce yapılan yanlışa göre uygun case seçilir.Daha sonra Süreye göre Çeşitli puanlama yapılır.
+        switch (mistake) {
+            case 5 :
+            { if (0<time && time<20) return 100;
+            else if (20<time && time<45)  return 90;
+            else return 85;
+            }
+            case 4 :
+            {   if (0<time && time<50) return 80;
+                else return 70;
+            }
+            case 3 :
+            {
+                if (0<time && time<60) return 60;
+                else return 50;
+            }
+            case 2 :
+            {
+                if (0<time && time<75) return 45;
+                else return 40;
+            }
+            case 1 :
+            {
+                if (0<time && time<90) return 35;
+                else return 30;
+            }
+            default: return 0;
+    }
+}
 }
